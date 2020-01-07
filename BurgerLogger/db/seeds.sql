9d90811c528b347c4990f786b233225663059e8f
David Gehtman <davidgehtman.91@gmail.com>
2020-01-07 14:27:17 -0500

    created the database and the seed file

--- BurgerLogger/db/seeds.sql
+++ BurgerLogger/db/seeds.sql
@@ -1,1 +1,10 @@
 -- insert burgers here
+INSERT INTO burgers (burger_name) VALUES ('Chipolte FireCracker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Canadian Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Brisket Burger', TRUE);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Baconator Burger', TRUE);
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Shake Shack Burger', TRUE);
+INSERT INTO burgers (burger_name) VALUES ('The Quadruple Bypass Burger');
